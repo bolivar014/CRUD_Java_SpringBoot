c9630671f9ff969cbe595bdd99957c1867cea26a
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-05-21 20:30:18 -0500

    creando modelo usuario

--- demo/src/main/java/com/example/demo/models/UsuarioModel.java
+++ demo/src/main/java/com/example/demo/models/UsuarioModel.java
@@ -1,14 +1,24 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuario")
 public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private Long id;
+
     private String nombre;
+    
     private String email;
+    
     private Integer prioridad;
 
     public void setPrioridad(Integer prioridad) {
